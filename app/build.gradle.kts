36b0024a890cbc504a44caab28f8289801f76c25
kajalkumari1112 <kajalsingh6430@gmail.com>
2026-04-28 21:45:13 +0530

    chore: bump version to 1.2.0 (code: 10200)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.cryptopricetracker"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 10200
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
